23b6953e2ebde96dec858f0e3cdd8e2f0d74717d
Will Gantt <wgantt.iv@gmail.com>
2020-09-10 16:00:02 -0400

    Add setup.py, requirements.txt (haven't tested though)

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="event_type_induction",
+    version="0.1",
+    description="Event type induction and predction on Universal Decompositional Semantics",
+    author="Will Gantt, Aaron Steven White",
+    author_email="wgantt@cs.rochester.edu, aaron.white@rochester.edu",
+    license="MIT",
+    packages=find_packages(),
+    package_dir={"event_type_induction": "event_type_induction"},
+    install_requires=[
+        "decomp==2.0.0a1",
+        "glob==0.2.*",
+        "networkx==2.5.*",
+        "numpy==1.17.*",
+        "overrides==3.1.*",
+        "setuptools==49.6.*",
+        "torch==0.4.*",
+        "typing==3.7.*",
+    ],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    zip_safe=False,
+)
